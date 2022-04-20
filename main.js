77e882ce07d2a411105d18b7f4a72740ec49ecc9
Daun Jeong <qkrtjrfo1@naver.com>
2022-04-20 17:40:21 +0900

    (JS) delete 까지 완료

--- main.js
+++ main.js
@@ -1,29 +1,68 @@
-let inputBox = document.getElementById('input-box')
-let plusBtn = document.getElementById('plus-btn')
-let checkBtn = document.getElementById('check-btn')
-let taskList=[];
+let inputBox = document.getElementById('input-box');
+let plusBtn = document.getElementById('plus-btn');
+let checkBtn = document.getElementById('check-btn');
+let taskInput=[];
+let taskBoard = document.getElementById('task-board');
 
-
-plusBtn.addEventListener("click",addTask)
+plusBtn.addEventListener("click",addTask);
 
 function addTask(){
-    let inputVal = inputBox.value;
-    taskList.push(inputVal)
-    console.log(taskList)
-    render()
+  let task = {
+    id : randomId(),
+    inputVal : inputBox.value,
+    isComplete : false,
+  }
+  taskInput.push(task)
+  console.log(taskInput)
+  render();
 }
 
 function render(){
-    let resultHTML ='';
-     
-    for(let i=0; i<taskList.length;i++){
-        resultHTML += `<div class="task-item">
-        <div>${taskList[i]} </div>
-        <div>
-          <button id="check-btn">Check</button>
-          <button>Delete</button>
-        </div>
-      </div>`
+  let resultHTML = '';
+  for(let i=0; i<taskInput.length;i++){
+
+    if(taskInput[i].isComplete == true){
+      resultHTML+=`<div class="task-item">
+    <div class="completeTrue">${taskInput[i].inputVal}</div>
+    <div>
+      <button onclick="taskDone('${taskInput[i].id}')">Check</button>
+      <button onclick="taskDelete('${taskInput[i].id}')">Delete</button>
+    </div>
+  </div>`
+    } else {
+
+      resultHTML+=`<div class="task-item">
+    <div>${taskInput[i].inputVal}</div>
+    <div>
+      <button onclick="taskDone('${taskInput[i].id}')">Check</button>
+      <button  onclick="taskDelete('${taskInput[i].id}')">Delete</button>
+    </div>
+  </div>`
     }
-    document.getElementById('task-board').innerHTML = resultHTML 
+    
+  }
+  document.getElementById('task-board').innerHTML = resultHTML;
+}
+
+function randomId(){
+  return '_' + Math.random().toString(36).substr(2, 9);
+}
+
+function taskDone(id){
+  
+  for(let i=0;i<taskInput.length; i++){
+    if(taskInput[i].id == id){
+      taskInput[i].isComplete = !taskInput[i].isComplete
+      break;
+    }
+  } render();
+}
+
+function taskDelete(id){
+  for(let i=0;i<taskInput.length; i++){
+    if(taskInput[i].id == id){
+      taskInput.splice(i,1)
+      break;
+    }
+  } render();
 }
